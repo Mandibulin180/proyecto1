ac74e6b80d60a0ba58d321fcba9566e00f030e93
Gonzalo.D <gonzaloagustindelgado@gmail.com>
2024-02-29 10:04:14 -0300

    Circuit Breaker, config Actuator

--- orders-service/src/main/java/com/orders_service/controller/OrderController.java
+++ orders-service/src/main/java/com/orders_service/controller/OrderController.java
@@ -11,11 +11,13 @@ import com.orders_service.dto.OrderRequest;
 import com.orders_service.dto.OrderResponse;
 import com.orders_service.service.OrderService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 @RestController
@@ -28,9 +30,10 @@ public class OrderController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @PreAuthorize("hasRole('admin')")
-    public String makeOrder(@RequestBody OrderRequest orderRequest){
-        this.orderService.placeOrder(orderRequest);
-        return "Order placed successfully";
+    @CircuitBreaker(name ="orders-service",fallbackMethod = "placeOrderFallback")
+    public ResponseEntity<OrderResponse> makeOrder(@RequestBody OrderRequest orderRequest){
+        var order = this.orderService.placeOrder(orderRequest);
+        return ResponseEntity.ok(order);
     }
 
     @GetMapping
@@ -38,6 +41,10 @@ public class OrderController {
     @PreAuthorize("hasRole('user')")
     public List<OrderResponse> getAllOrders(){
         return this.orderService.getAllOrders();
+    }
+
+    private ResponseEntity<OrderResponse> placeOrderFallback(OrderRequest orderRequest,Throwable throwable){
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build();
     }
     
 }
--- orders-service/src/main/java/com/orders_service/service/OrderService.java
+++ orders-service/src/main/java/com/orders_service/service/OrderService.java
@@ -24,7 +24,7 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
 
-    public void placeOrder(OrderRequest orderRequest){
+    public OrderResponse placeOrder(OrderRequest orderRequest){
         // TODO check for inventory
 
         BaseResponse result = this.webClientBuilder.build()
@@ -40,7 +40,8 @@ public class OrderService {
             order.setOrdernumber(UUID.randomUUID().toString());
             order.setOrderItems(orderRequest.getOrderItems().stream()
             .map(orderItemRequest -> mapOrderItemRequestToOrderItem(orderItemRequest,order)).toList());
-            this.orderRepository.save(order);
+            var savedOrder = this.orderRepository.save(order);
+            return mapToOrderResponse(savedOrder);
         }else{
             throw new IllegalArgumentException("Some of the products are not in stock");
         }
